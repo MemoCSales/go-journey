d552e3ea1903a8f636929655e7df9c78fa0f1285
Guillermo Sales <guillermosales@MBP-de-Jimena.fritz.box>
2025-06-24 23:20:35 +0200

    Add: gitignore, TDD on iteration, adding more Example and reviewing Benchmark

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,19 +1,61 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+const repeatCount = 5
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	assertIteration := func(t testing.TB, got, want string) {
+		t.Helper() 	// Marca esta función como una función auxiliar de prueba, no una prueba en sí misma
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+	t.Run("Repeating 5 times", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+		assertIteration(t, repeated, expected)
+	})
 
-	if repeated != expected {
-		t.Errorf("expected '%q' but got '%q'", expected, repeated)
+	t.Run("Repeating 0 times", func(t *testing.T) {
+		repeated := Repeat("a", 0)
+		expected := ""
+		assertIteration(t, repeated, expected)
+	})
+}
+
+func Benchmark(b *testing.B) {
+	for b.Loop() {
+		Repeat("a", 5)
 	}
 }
 
-func Repeat(c string) (repeat_output string) {
-	for i := 0; i < 5; i++ {
-		repeat_output += c
+func Repeat(character string, count int) string {
+	var repeated strings.Builder
+	for i := 0; i < count; i++ {
+		repeated.WriteString(character)
 	}
-	return
+	return repeated.String()
+}
+
+func ExampleRepeat() {
+	repeat := Repeat("a", 5)
+	fmt.Println(repeat)
+	// Output: aaaaa
+}
+
+func ExampleRepeat_zero() {
+	repeat := Repeat("b", 0)
+	fmt.Println(repeat)
+	// Output: 
+}
+
+func ExampleRepeat_multipleCharacters() {
+	repeat := Repeat("abc", 3)
+	fmt.Println(repeat)
+	// Output: abcabcabc
 }
