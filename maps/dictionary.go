214281097e14848affa76966e02cb84b60b5e7a6
Guillermo Sales <guillermosales@MBP-de-Jimena.fritz.box>
2025-07-10 21:56:26 +0200

    Refactor

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,15 +1,37 @@
 package main
 
-import "errors"
-
 type Dictionary map[string]string
+type DictionaryErr string
+
+const (
+	ErrNotFound = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+)
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	// map lookup returns 2 values. The 2nd is boolean which indicates if the key was found successfully
 	definition, ok := d[word]
 
 	if !ok {
-		return "", errors.New("could not find the word you were looking for")
+		return "", ErrNotFound
 	}
 	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err 
+	}
+	return nil
 }
