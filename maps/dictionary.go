a1c0978c91e4259c220e0c627cfb6329b1e36596
Guillermo Sales <guillermosales@MBP-de-Jimena.fritz.box>
2025-07-23 19:26:58 +0200

    Add: Update and Delete functions along with their test cases

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -6,12 +6,42 @@ type DictionaryErr string
 const (
 	ErrNotFound = DictionaryErr("could not find the word you were looking for")
 	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExists = DictionaryErr("cannot update word because it does not exists")
 )
+
+func (d Dictionary) Delete(word string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExists
+	case nil:
+		delete(d, word)
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExists
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+	return nil
+}
 
 func (e DictionaryErr) Error() string {
 	return string(e)
 }
 
+// If successful Search return nil
 func (d Dictionary) Search(word string) (string, error) {
 	// map lookup returns 2 values. The 2nd is boolean which indicates if the key was found successfully
 	definition, ok := d[word]
